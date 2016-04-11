b7e168bc7061dccf7c31d94b959f1a16407a014b
Sijan Gurung <sijan.gurung@shortcut.no>
2016-04-11 12:43:55 +0200

    added base and mail layout

---
+++ MaterialTest/app/src/main/java/no/shortcut/materialtest/main/MainApplication.java
@@ -0,0 +1,12 @@
+package no.shortcut.materialtest.main;
+
+import com.karumi.rosie.application.RosieApplication;
+
+/**
+ * Created by Sijan Gurung on 11/04/16.
+ * Shortcut AS
+ * sijan.gurung@shortcut.no
+ */
+public class MainApplication extends RosieApplication {
+
+}

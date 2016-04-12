ad2ba7adc2b1f7f5a343ec0d3dfbd3892e1b2014
Ivan Hjelmeland <ivan.hjelmeland@shortcut.no>
2016-04-12 09:16:29 +0200

    Project compiles and runs with i simple "Hello World" in MainActivity.

--- MaterialTest/app/src/main/java/no/shortcut/materialtest/ApplicationModule.java
+++ MaterialTest/app/src/main/java/no/shortcut/materialtest/ApplicationModule.java
@@ -2,17 +2,22 @@ package no.shortcut.materialtest;
 
 import android.content.Context;
 
+import com.squareup.picasso.Picasso;
+
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
+import no.shortcut.materialtest.main.view.MainActivity;
 
 /**
  * Created by Ivan on 11/04/16.
  */
 
 @Module(
+        library = true,
         injects = {
+                MainActivity.class,
                 Application.class
         }
 )
@@ -29,5 +34,12 @@ public class ApplicationModule {
     public Context provideApplicationContext() {
         return app;
     }
+
+    @Provides
+    @Singleton
+    public Picasso providePicasso(Context context) {
+        return Picasso.with(context);
+    }
 }
+
 

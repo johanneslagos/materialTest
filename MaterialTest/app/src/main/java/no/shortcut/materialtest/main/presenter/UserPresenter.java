1fd05943375ccb10a91a4c85ae00ab33eff8ce72
Sijan Gurung <sijan.gurung@shortcut.no>
2016-04-12 15:50:28 +0200

    * Cleaning the projects - removed Rosie and other unnecessary plugins - cleaned the project folders

--- MaterialTest/app/src/main/java/no/shortcut/materialtest/main/presenter/UserPresenter.java
+++ MaterialTest/app/src/main/java/no/shortcut/materialtest/main/presenter/UserPresenter.java
@@ -1,9 +1,10 @@
-package no.shortcut.materialtest.main.model;
+package no.shortcut.materialtest.main.presenter;
 
 /**
  * Created by Sijan Gurung on 11/04/16.
  * Shortcut AS
  * sijan.gurung@shortcut.no
  */
-public class Artist{
+public class UserPresenter{
+
 }
--- MaterialTest/app/src/main/java/no/shortcut/materialtest/main/view/MainActivity.java
+++ MaterialTest/app/src/main/java/no/shortcut/materialtest/main/view/MainActivity.java
@@ -1,14 +1,7 @@
 package no.shortcut.materialtest.main.view;
 
-import com.karumi.rosie.view.RosieAppCompatActivity;
+import android.support.v7.app.AppCompatActivity;
 
-import no.shortcut.materialtest.R;
-
-public class MainActivity extends RosieAppCompatActivity {
-
-    @Override
-    protected int getLayoutId() {
-        return R.layout.activity_main;
-    }
+public class MainActivity extends AppCompatActivity{
 
 }
